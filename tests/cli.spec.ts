cd64c2974fbd95e1043513ed703d2541ef0c3c4c
henryruhs <info@henryruhs.com>
2023-01-11 14:27:32 +0100

    Testing for main command

--- tests/cli.spec.ts
+++ tests/cli.spec.ts
@@ -1,8 +1,22 @@
+import fs from 'fs';
 import { expect } from 'chai';
 import { exec } from 'child_process';
 
 describe('cli', () =>
 {
+	it('run main command', done =>
+	{
+		exec('node --loader ts-node/esm src/cli.ts --path=tests/provider --amount=2', error =>
+		{
+			expect(error).to.be.null;
+			expect(fs.readdirSync('tests/__provider__0')).to.be.eql([ '01', '02', '03', '04' ]);
+			expect(fs.readdirSync('tests/__provider__1')).to.be.eql([ '04' ]);
+			fs.rmSync('tests/__provider__0', { recursive: true });
+			fs.rmSync('tests/__provider__1', { recursive: true });
+			done();
+		});
+	});
+
 	it('run help command', done =>
 	{
 		exec('node --loader ts-node/esm src/cli.ts --help', error =>
